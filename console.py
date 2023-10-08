ad31f39c50e55aaec330e297f3f8c18c7956ea9a
0minutes <107317605+0minutes@users.noreply.github.com>
2023-10-08 18:24:52 +0100

    Add files via upload

--- console.py
+++ console.py
@@ -14,10 +14,10 @@ class ConsoleApp:
         # Create a Text widget to display text
         self.output_text = tk.Text(self.console)
         self.output_text.pack(side=tk.TOP, fill="both", expand=True)
-        self.output_text.config(state=tk.DISABLED, bg=DEFAULT_COLOR, font=DEFAULT_FONT, fg=FONT_COLOR)  # Set font color
+        self.output_text.config(state=tk.DISABLED, bg=DEFAULT_COLOR, font=DEFAULT_FONT, fg=FONT_COLOR)
         
         # Create an Entry widget for text input
-        self.text = tk.Entry(self.console, bg=DEFAULT_COLOR, font=DEFAULT_FONT, fg=FONT_COLOR)  # Set font color
+        self.text = tk.Entry(self.console, bg=DEFAULT_COLOR, font=DEFAULT_FONT, fg=FONT_COLOR)
         self.text.pack(side=tk.BOTTOM, fill="both")
 
         # Remove the white borders around the input text widget
@@ -35,16 +35,14 @@ class ConsoleApp:
         
         text = info[1]
         color = info[0]
-
-        print(f"DEBUG: {text = }, {color = }, {self.text.get() = }")
         
-        self.output_text.config(state=tk.NORMAL, fg=color)  # Set font color
+        self.output_text.config(state=tk.NORMAL, fg=color)
         self.output_text.insert(tk.END, str(text) + "\n")
         self.output_text.config(state=tk.DISABLED)
 
         self.output_text.see(tk.END)
         
-        self.text.delete(0, tk.END)  # Clear the input text box
+        self.text.delete(0, tk.END)
 
     def clear_console(self):
         self.output_text.config(state=tk.NORMAL) 
@@ -54,7 +52,7 @@ class ConsoleApp:
 if __name__ == "__main__":
     DEFAULT_COLOR = "#404040"
     DEFAULT_FONT = ("Tahoma", 10)
-    FONT_COLOR = "white"  # Change this to your desired font color
+    FONT_COLOR = "white"
     
     console = tk.Tk()
     app = ConsoleApp(console)
